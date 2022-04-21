25ffbf5941a584a0274392090b4860236bbdad24
sarwadnya1996 <sarwadnya1996@gmail.com>
2022-04-21 19:55:57 +0530

    conventional linear search code added

---
+++ src/LinearSearch.java
@@ -0,0 +1,17 @@
+/**
+ * Time complexity-->O(n)
+ */
+public class LinearSearch {
+    public static int search(int[] arr,int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] inputArr=new int[]{10,34,67,97,1};
+        System.out.println(search(inputArr,97));
+    }
+}
